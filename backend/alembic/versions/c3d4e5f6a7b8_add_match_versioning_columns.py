e108348c2b5c085f01b5a02e9c25f4ef774f2bea
Vivan Sharma <vivans720@gmail.com>
2026-09-15 01:08:03 +0530

    alembic bug fixes

--- backend/alembic/versions/c3d4e5f6a7b8_add_match_versioning_columns.py
+++ backend/alembic/versions/c3d4e5f6a7b8_add_match_versioning_columns.py
@@ -1,7 +1,7 @@
 """add_match_versioning_columns
 
-Revision ID: b2c3d4e5f6a7
-Revises: a1b2c3d4e5f6
+Revision ID: c3d4e5f6a7b8
+Revises: b2c3d4e5f6a7
 Create Date: 2026-09-14 17:00:00.000000
 
 """
@@ -11,8 +11,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b2c3d4e5f6a7'
-down_revision: Union[str, Sequence[str], None] = 'a1b2c3d4e5f6'
+revision: str = 'c3d4e5f6a7b8'
+down_revision: Union[str, Sequence[str], None] = 'b2c3d4e5f6a7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
